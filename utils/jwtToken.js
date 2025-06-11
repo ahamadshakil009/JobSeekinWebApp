7121afd5fb60ab9370eb9d52fd7a4675a85fb86e
shakil <ahamadshakil2000@gmail.com>
2025-06-12 01:14:38 +0530

    Fix: Updated backend logic / env / routes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,11 +1,13 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
-  // const options = {
-  //   expires: new Date(
-  //     Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-  //   ),
-  //   httpOnly: true, // Set httpOnly to true
-  // };
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, // Set httpOnly to true
+    secure: true,
+    sameSite: "None",
+  };
 
   res.status(statusCode).cookie("token", token, {
     httpOnly:true,
